24e4e6066837528dc81c5401869bc6dfd9ece085
root <nitanka>
2016-12-08 03:39:35 +0530

    script to find a word inside a sentence

---
+++ findString.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# python script to find the a word among the words present in the input sentence
+
+inputString = input("Input the sentence :  ")
+findString = input ("Input the word to find :  ")
+
+if len(findString.split()) != 1:
+    print("Please enter a single word!!!")
+    exit
+else:
+    for eachWord in inputString.split():
+        if findString == eachWord:
+            print("The word to find is present in the input sentence")
+
+
