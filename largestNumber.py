2ca0c75b09a0b6378cb253b6822be1984174b5ad
root <nitanka>
2016-12-07 10:17:53 +0530

    Initial script, need to complete

---
+++ largestNumber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# python script to find the largest number from a given list of numbers
+
+def largest (insertList):
+    large = 0
+    for i=0
+
+
+totalElements = int(input("Enter the total number of elements in the list :  "))
+inputList = []
+count = 0
+
+while count < totalElements:
+    element = input("Enter the number {}  ".format(count + 1))
+    inputList.append(element)
+    count += 1
