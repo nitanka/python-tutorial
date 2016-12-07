0f43c4dd72cb1957322feecd4504535658cf49c9
root <nitanka>
2016-12-08 04:11:40 +0530

    Fixed the issue with the exit

--- findString.py
+++ findString.py
@@ -2,12 +2,14 @@
 
 # python script to find the a word among the words present in the input sentence
 
+from sys import exit
+
 inputString = input("Input the sentence :  ")
 findString = input ("Input the word to find :  ")
 
 if len(findString.split()) != 1:
     print("Please enter a single word!!!")
-    exit
+    exit()
 else:
     for eachWord in inputString.split():
         if findString == eachWord:
