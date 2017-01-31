6c82d7b080f9d5edd7703a85faaec0f5b0ad0b44
root <nitanka>
2017-01-31 10:45:08 +0530

    Change in the print statement

--- stringLen.py
+++ stringLen.py
@@ -9,4 +9,4 @@ count = 0 # to store the number of character in the word
 for char in word:  # for each character in the word
     count += 1     # increment the count of the number of character
 
-print ("The length of the word is {}".format(count))
+print ("The length of the word {} is {}".format(word,count))
