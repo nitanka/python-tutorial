d26dc4ded3f4493cb0fe4cd126b52dd5717f6df6
sajeshkumar-rzt <nitanka.gogoi@razorthink.net>
2017-02-05 12:50:34 +0530

    added exception handling in the largestNumber.py

--- largestNumber.py
+++ largestNumber.py
@@ -2,16 +2,43 @@
 
 # python script to find the largest number from a given list of numbers
 
+from sys import exit;
+
 def largest (insertList):
     large = 0
-    for i=0
+    for ELEMENT in insertList:
+        if insertList[large] < ELEMENT:
+            large = insertList.index(ELEMENT)
+    return large
+    
 
+# Obtaining total number of elements in the list
+try:
+    totalElements = int(input("Enter the total number of elements in the list :  "))
+except ValueError as ve:
+    print("Please enter integers")
+    print("Program Exiting!!!!!!!!!!!")
+    exit()
 
-totalElements = int(input("Enter the total number of elements in the list :  "))
-inputList = []
-count = 0
+if totalElements <= 0:
+    print("Please enter values greater than 0")
+    print("Program Exiting!!!!!!!!!!!")
+    exit()
+
+inputList = [] #Empty list to hold the integer list given by the user
+count = 0      # counter to use for obtaining list items from the user. counter >= totalElements
 
 while count < totalElements:
-    element = input("Enter the number {}  ".format(count + 1))
+    try:
+        element = int(input("Enter the number {}  ".format(count + 1)))
+    except ValueError as ve:
+        print("Enter integer number")
+        continue 
+        
     inputList.append(element)
     count += 1
+
+
+LARGEST = largest(inputList)
+
+print("The largest number in the input list is {}".format(inputList[LARGEST]))
