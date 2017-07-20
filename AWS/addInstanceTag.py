7edc378b41f67f962aaf3c03f479125da1fb9b17
nitanka <nitankagogoi@gmail.com>
2017-07-20 19:32:54 +0530

    added script to add new tags to instances on instance filtered based on tag

--- AWS/addInstanceTag.py
+++ AWS/addInstanceTag.py
@@ -1,1 +1,20 @@
 #!/bin/env python
+
+import sys
+import boto3
+import ast 
+
+Region = sys.argv[1]
+selectionTag = sys.argv[2]
+selectionValue = sys.argv[3].split()
+addTag = list(ast.literal_eval(sys.argv[4]))
+
+ec2 = boto3.client('ec2',Region)
+response = ec2.describe_instances(Filters=[{'Name':'tag:' + selectionTag, 'Values': selectionValue }])
+
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        if instance['State']['Name'] == 'running':
+           for tagDict in addTag:
+               ec2.create_tags(Resources=[instance['InstanceId']], Tags=[{'Key': tagDict['Key'], 'Value': tagDict['Value']}])
+
