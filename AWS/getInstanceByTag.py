c610e911ffc327c81e21a4d640dfa2e95523199c
nitanka <nitankagogoi@gmail.com>
2017-07-20 17:34:47 +0530

    added options for tag value

--- AWS/getInstanceByTag.py
+++ AWS/getInstanceByTag.py
@@ -4,9 +4,10 @@ import boto3
 import sys
 Region = sys.argv[1]
 Tag = sys.argv[2]
+tagVALUE = sys.argv[3].split()
 
 client = boto3.client('ec2',Region)
-response = client.describe_instances(Filters=[{'Name':'tag:'+Tag,'Values':['nitanka-test']}])
+response = client.describe_instances(Filters=[{'Name':'tag:' + Tag,'Values':tagVALUE}])
 
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
