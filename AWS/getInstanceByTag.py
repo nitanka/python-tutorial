edf13c0218be0a125de8f56aebe48e7a24fde3ea
nitanka <nitankagogoi@gmail.com>
2017-07-28 15:12:09 +0530

    changed the filter to have support for running instances

--- AWS/getInstanceByTag.py
+++ AWS/getInstanceByTag.py
@@ -7,9 +7,8 @@ Tag = sys.argv[2]
 tagVALUE = sys.argv[3].split()
 
 client = boto3.client('ec2',Region)
-response = client.describe_instances(Filters=[{'Name':'tag:' + Tag,'Values':tagVALUE}])
+response = client.describe_instances(Filters=[{'Name':'tag:' + Tag,'Values':tagVALUE},{'Name':'instance-state-name', 'Values':['running']}])
 
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-       print(instance['State']['Name']) == 'running'
-       print(instance['PublicIpAddress'] + '  :  ' + instance['PrivateIpAddress'])
+        print(instance['PublicIpAddress']  + '  :  ' + instance['PrivateIpAddress'])
